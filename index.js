c4c1b3da8fca31cdc61b9b4209575006a690bb5e
Sathuzan <69593398+Sathuzan23@users.noreply.github.com>
2025-09-30 10:35:23 +0530

    Add Netlify app URL to CORS allowed origins

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:3000",
       "http://127.0.0.1:5173",
+      "https://sathuzan-refine-dashboard.netlify.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
